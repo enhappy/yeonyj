8f4e4da2d3d18dcd3f105754557f0c4e5c427bb6
enhappy <enhappy@daum.net>
2023-02-11 16:45:32 +0900

    메인메뉴 수정, 숍링크추가, ico변경

--- js/flickr.js
+++ js/flickr.js
@@ -1,31 +1,83 @@
-
-
-
 const base = 'https://www.flickr.com/services/rest/?';
 const method = 'flickr.interestingness.getList';
-//const method2 = 'flickr2.interesti2ngness.getLis2t';
 const method2 = 'flickr.photos.search';
 const key = '9a7d9833ee775fa36568354172a2f1cd';
 const per_page = 8;
 const frame = document.querySelector('#list');
 
+const loading = document.querySelector('.loading');
+const input = document.querySelector('#search');
+const btn = document.querySelector('.btnSearch');
+const gal = document.querySelector('#gallery');
 const url = `${base}method=${method}&api_key=${key}&per_page=${per_page}&format=json&nojsoncallback=1`;
-//const url2 = `${base}method=${method2}&api_key=${key}&per_page=${per_page}&format=json&nojsoncallback=1`;
-const url2 = `${base}method=${method2}&api_key=${key}&per_page=${per_page}&format=json&nojsoncallback=1&tags=tea&privacy_filter=1`;
+const url2 = `${base}method=${method2}&api_key=${key}&per_page=${per_page}&format=json&nojsoncallback=1&tags=car&privacy_filter=1`;
 
+//초기 갤러리 화면에 보여지는 데이터를 불러준다
 callData(url2);
 
+
+
+//이벤트위임으로 각 이미지 클릭시 큰이미지를 호출해서 보여주기
+frame.addEventListener('click', (e) => {
+	e.preventDefault();
+
+	//클릭한 대상이 #list이면 return하도록
+	if (e.target == frame) return;
+	//범위를 완전 좁혀서 썸네일을 클릭해야지만 사진을 볼수있도록
+	let target = e.target.closest('.item').querySelector('.thumb');
+
+	//사용자가 클릭한 대상이 위에 변수로 잡은 썸네일인지를 묻고 그렇다면 그때 href속성을 가져오도록
+	if (e.target == target) {
+		//let imgSrc = target.parentElement.getAttribute('href'); //thumb의 부모인 a태그를 찾는 방법
+		let imgSrc = target.closest('a').getAttribute('href'); //thumb에서 가장 가까이에 있는 a태그를 찾는 방법
+		let pop = document.createElement('aside');
+		pop.classList.add('pop');
+		let pops = `
+		<img src="${imgSrc}">
+		<span class="close">Close</span>
+	`;
+		pop.innerHTML = pops;
+		gal.append(pop);
+	} else {
+		return;
+	}
+});
+
+//생성되지 않은 미래의 DOM요소를 선택하는게 방법이 사실은 없다. => react에서는 가상돔
+gal.addEventListener('click', (e) => {
+	//애매하게 aside를 찾는게 아니라 정확하게 pop이라는 클래스를 추적한다
+	let pop = gal.querySelector('.pop');
+	if (pop != null) {
+		//탑이 존재하는지를 정확하게 선후관계를 조건에 넣어주는것
+		let close = pop.querySelector('.close');
+		if (e.target == close) pop.remove();
+	} //pop.length > 0 length는 배열이나 문자열의 길이를 찾는것
+	//	let target = e.target.closest('aside');
+	//	target.remove();
+});
+
 function callData(url) {
+	//기존에 있는 html을 모두 제거
+	frame.innerHTML = '';
+	//로딩 이미지를 off클래스를 제거함으로 다시 출력
+	loading.classList.remove('off');
+	//플레임 on클래스를 지워서 활성화 모션전으로 되돌림
+	frame.classList.remove('on');
+
 	fetch(url)
 		.then((data) => {
 			return data.json();
 		})
 		.then((json) => {
 			let items = json.photos.photo;
-			// console.log(items);
-			createList(items);
+			console.log(items);
 
-			delayLoading();
+			if (items.length > 0) {
+				createList(items);
+				delayLoading();
+			} else {
+				alert('검색하신 이미지의 데이터가 없습니다');
+			}
 		});
 }
 
@@ -41,7 +93,7 @@ function createList(items) {
         <li class="item">
           <div>
             <a href=${imgSrcBig}>
-              <img src=${imgSrc}>
+              <img class="thumb" src=${imgSrc}>
             </a>
             <p>${el.title}
             </p>
@@ -62,10 +114,17 @@ function delayLoading() {
 			count++;
 			if (count == len) isoLayout();
 		});
+		//만약 img DOM요소에 이미지 소스가 없거나 로드에 에러가나서 엑박이뜨면 1. 단계 아예 이미지 자체를 display : none으로 없애는 방법 2. 단계 src속성에 접근해서 대체이미지로 바꾸는 방법
+
+		el.addEventListener('error', (e) => {
+			//e.currentTarget.closest('.item').style.display = 'none';
+			e.currentTarget.closest('.item').querySelector('img').setAttribute('src', 'img/noImage.png');
+		});
 	}
 }
 
 function isoLayout() {
+	loading.classList.add('off');
 	frame.classList.add('on');
 	new Isotope('#list', {
 		itemSelection: '.item',
---
+++ js/validation.js
@@ -0,0 +1,182 @@
+const form = document.querySelector('#member');
+const btnSubmit = form.querySelector('input[type=submit]');
+
+btnSubmit.addEventListener('click', (e) => {
+	// 조건문으로 해당 input의 유효성검사를 한 후 거짓이면?? => e.preventDefault();
+	// if(!유효성 검사함수) e.preventDefault();
+
+	if (!isTxt('userid')) e.preventDefault();
+	if (!isTxt('comments', 20)) e.preventDefault();
+
+	if (!isEmail('email')) e.preventDefault();
+	if (!isCheck('gender')) e.preventDefault();
+	if (!isCheck('hobby')) e.preventDefault();
+
+	if (!isSelect('edu')) e.preventDefault();
+	if (!isPwd('pwd1', 'pwd2', 5)) e.preventDefault();
+});
+
+/*
+동적언어 - 자바스크립트 
+
+let ani = 5;
+어떤 변수든간에 상관없이 다 선언할수있음
+단점 : 만약 변수의 자료형등의 오류가나면 알려주지않음, 찾기어려움
+
+정적언어 - 자바
+
+int ani = 5;
+string ani = "gogo";
+변수선언시 자료형을 제대로 선언하지 않으면 에러
+장점 : 같은 상황에서 오류가 정확하게 나므로 해결이 쉽다
+*/
+
+//1. type이 text인 form요소의 인증함수 (유효성검사)
+
+function isTxt(el, len) {
+	if (len === undefined) len = 5;
+	let input = form.querySelector(`[name=${el}]`);
+	//name값이 매개변수로 넣은 이름인 input을 찾아서
+	let txt = input.value;
+	//해당 input에 사용자가 작성한 값(value)을 txt라는 변수에 담는것
+
+	if (txt.length >= len) {
+		// 참인경우에 추가적으로 들어갈 코드
+		// 참이면 에러메시지가 필요없으므로 p를 찾아서 지우는 방법
+		const errMsgs = input.closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) input.closest('td').querySelector('p').remove();
+
+		return true;
+	} else {
+		//1 에러메세지를 일단 지운뒤 아래에서 다시 작성하게 하는 방법
+		const errMsgs = input.closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) input.closest('td').querySelector('p').remove();
+
+		//2 이미 적혀진 에러메세지가 있는지 확인한 후 아래의 새로 생성하는 p태그의 코드를 만나지 않고 바로 false를 반환하도록 하는 방법
+		// const errMsgs = input.closest('td').querySelectorAll('p');
+		// if (errMsgs.len > 0) return false;
+		//return을 이용하여 에러머시지가 존재하면 구문을 중단시킴으로 하단 구문(p태그 생성코드)를 실행하지 않고 false를 반환시키게 함
+
+		/*
+    return???
+    1. 구역 밖으로 이후에 적힌 값을 반환하는 용도 <== 반환의 목적
+    2. 리턴구문을 만나면 더이상 아래의 코드가 진행되지 않고 멈춘뒤 이후 적힌 값을 반환해버린다 <== 멈추는 용도
+    */
+
+		//거짓인경우에 추가적으로 들어갈 코드 = > 경고문구를 작성해서 안내를 해줘야한다
+		//하지만 이와 같이 하게되면 에러메세지가 반복적으로 계속 출력되는 문제가 있다  해결은 => 윗줄
+		let errMessage = document.createElement('p');
+		errMessage.append(`입력항목을 ${len}글자 이상 입력하셔야 합니다.`);
+		input.closest('td').append(errMessage);
+		return false;
+		//오류가 날때마다 계속적으로 p태그를 만들어서 작성하는 문제가 발생
+	}
+}
+
+//2. email인증함수
+function isEmail(el) {
+	let input = form.querySelector(`[name=${el}]`);
+	let txt = input.value;
+
+	//@가 있는지 없는지??
+	if (/@/.test(txt)) {
+		const errMsgs = input.closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) input.closest('td').querySelector('p').remove();
+
+		return true;
+	} else {
+		const errMsgs = input.closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) return false;
+
+		let errMessage = document.createElement('p');
+		errMessage.append('@를 포함한 전체 이메일 주소를 입력하세요.');
+		input.closest('td').append(errMessage);
+		return false;
+	}
+}
+//3. check인증함수
+function isCheck(el) {
+	let inputs = form.querySelectorAll(`[name=${el}]`);
+	let isCheck = false;
+
+	//inputs배열을 반복을 돌면서 체크가 되어있는지를 살피고 체크가 되어있다면 false를 true로 변경
+	for (let el of inputs) {
+		if (el.checked) isCheck = true;
+	}
+
+	if (isCheck) {
+		const errMsgs = inputs[0].closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) inputs[0].closest('td').querySelector('p').remove();
+
+		return true;
+	} else {
+		const errMsgs = inputs[0].closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) inputs.closest('td').querySelector('p').remove();
+
+		let errMessage = document.createElement('p');
+		errMessage.append('필수 입력 항목을 체크해 주세요');
+		inputs[0].closest('td').append(errMessage);
+		return false;
+	}
+}
+
+//4. selcet인증함수
+function isSelect(el) {
+	let sel = form.querySelector(`[name=${el}]`);
+
+	let sel_index = sel.options.selectedIndex;
+	//select요소에 접근해서 자식 요소인 option들 중에 선택되어진 index를 찾아서 해당 인덱스를 가지고 온다 (number)
+	let value = sel[sel_index].value;
+
+	// if(sel_index !== 0)
+	//value값이 비어있는지? 아닌지가 중요함
+	if (value !== '') {
+		const errMsgs = sel.closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) sel.closest('td').querySelector('p').remove();
+		return true;
+	} else {
+		const errMsgs = sel.closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) sel.closest('td').querySelector('p').remove();
+
+		let errMessage = document.createElement('p');
+		errMessage.append('항목을 선택해 주세요');
+		sel.closest('td').append(errMessage);
+		return false;
+	}
+}
+
+//5 password 인증함수
+function isPwd(el1, el2, len) {
+	let pwd1 = form.querySelector(`[name=${el1}]`);
+	let pwd2 = form.querySelector(`[name=${el2}]`);
+	let pwd1_value = pwd1.value;
+	let pwd2_value = pwd2.value;
+
+	//보통 패스워드는 숫자, 문자, 특수문자 조건을 정규표현식으로 변수로 저장
+	const num = /[0-9]/;
+	const eng = /[a-zA-Z]/;
+	const spc = /[~!@#$%^&*()_+?<>]/;
+
+
+	if (
+		pwd1_value === pwd2_value &&
+		pwd1_value.length >= len &&
+		num.test(pwd1_value) &&
+		eng.test(pwd1_value) &&
+		spc.test(pwd1_value)
+	) {
+		const errMsgs = pwd1.closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) pwd1.closest('td').querySelector('p').remove();
+
+		return true;
+	} else {
+		const errMsgs = pwd1.closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) return false;
+
+		let errMessage = document.createElement('p');
+		errMessage.append(`비밀번호는 ${len}글자 이상, 영문, 숫자, 특수문자를 포함하여 동일하게 입력하세요`);
+		pwd1.closest('td').append(errMessage);
+		return false;
+	}
+}
+
